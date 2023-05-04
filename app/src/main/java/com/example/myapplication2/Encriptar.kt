6ed4c7b462ad6e0b92de488878509b0490b80c55
nicolas nanjari <nicolascanales216@gmail.com>
2023-05-04 18:33:47 -0400

    4

---
+++ app/src/main/java/com/example/myapplication2/Encriptar.kt
@@ -0,0 +1,39 @@
+package com.example.myapplication2
+
+import android.util.Log
+import javax.crypto.Cipher
+import javax.crypto.KeyGenerator
+import javax.crypto.SecretKey
+import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
+
+class Encriptar {
+
+    private val key = generateKey()
+
+    fun encriptarname(nombre:String): ByteArray {
+        val plaintext = nombre.toByteArray()
+        val cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING")
+        cipher.init(Cipher.ENCRYPT_MODE, key)
+        val ciphertext: ByteArray = cipher.doFinal(plaintext)
+        val iv: ByteArray = cipher.iv
+        return ciphertext + iv
+    }
+
+    fun desencriptarname(ciphertext: ByteArray): String {
+        val iv = ciphertext.takeLast(16).toByteArray() // Extraer el vector de inicialización del cifrado
+        val cipher1 = Cipher.getInstance("AES/CBC/PKCS5PADDING")
+        cipher1.init(Cipher.DECRYPT_MODE, key, IvParameterSpec(iv))
+        val plaintext: ByteArray = cipher1.doFinal(ciphertext.dropLast(16).toByteArray())
+        return String(plaintext, Charsets.UTF_8)
+    }
+
+    private fun generateKey(): SecretKey {
+        val keygen = KeyGenerator.getInstance("AES")
+        keygen.init(256)
+        return keygen.generateKey()
+    }
+
+
+
+}
--- app/src/main/java/com/example/myapplication2/test.kt
+++ app/src/main/java/com/example/myapplication2/test.kt
@@ -1,8 +1,6 @@
 package com.example.myapplication2
 
 import android.os.Bundle
-import android.os.Parcel
-import android.os.Parcelable
 import android.util.Log
 import android.view.View
 import android.widget.Button
@@ -10,16 +8,12 @@ import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.android.volley.AuthFailureError
-import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
-import com.android.volley.VolleyError
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
-import javax.crypto.Cipher
-import javax.crypto.KeyGenerator
 import javax.crypto.SecretKey
-import javax.crypto.spec.IvParameterSpec
+import javax.crypto.spec.SecretKeySpec
 
 lateinit var btnconn : Button
 lateinit var txtnombre : EditText
@@ -44,6 +38,7 @@ class test() : ComponentActivity(), View.OnClickListener {
         txtnombre = findViewById(R.id.txtName)
         txtcoreo = findViewById(R.id.txtEmail)
         txtpass = findViewById(R.id.txtPass)
+
         btnconn.setOnClickListener (this)
     }
 
@@ -57,26 +52,13 @@ class test() : ComponentActivity(), View.OnClickListener {
             var correo = txtcoreo.getText().toString()
             var pass = txtpass.getText().toString()
 
-            //createUser(name,correo,pass)
-            val plaintext = name.toString().toByteArray()
-            val keygen = KeyGenerator.getInstance("AES")
-            keygen.init(256)
-            val key: SecretKey = keygen.generateKey()
+            createUser(name,correo,pass)
 
-            // Cifrar datos en modo CBC con relleno PKCS5
-            val cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING")
-            cipher.init(Cipher.ENCRYPT_MODE, key)
-            val ciphertext: ByteArray = cipher.doFinal(plaintext)
-            val iv: ByteArray = cipher.iv
-
-            Log.d("cifrado","${ciphertext.toString()}")
-
-            val cipher1 = Cipher.getInstance("AES/CBC/PKCS5PADDING")
-            cipher1.init(Cipher.DECRYPT_MODE, key, IvParameterSpec(iv))
-            val plaintext2: ByteArray = cipher1.doFinal(ciphertext)
-            val plaintextString = String(plaintext2, Charsets.UTF_8)
-            Log.d("TAG", plaintextString)
-
+            val encriptador = Encriptar()
+            val ciphertext = encriptador.encriptarname(name)
+            println("Mensaje encriptado: ${ciphertext.toString()}")
+            val plaintext = encriptador.desencriptarname(ciphertext)
+            println("Mensaje desencriptado: $plaintext")
 
         }
     }
@@ -85,23 +67,23 @@ class test() : ComponentActivity(), View.OnClickListener {
      */
     private fun createUser(name: String, correo: String, pass: String) {
         val stringRequest = object : StringRequest(
-            Method.POST, url,
-            Response.Listener<String> { response ->
-                Toast.makeText(this, "Correcto", Toast.LENGTH_SHORT).show()
-                txtnombre.text.clear()
-                txtcoreo.text.clear()
-                txtpass.text.clear()
-            },
-            Response.ErrorListener { error ->
-                println("nooo")
-            }
-        ) {
-            @Throws(AuthFailureError::class)
-            override fun getParams(): Map<String, String> {
-                val params = HashMap<String, String>()
-                params["nombre"] = name
-                params["email"] = correo
-                params["password"] = pass
+                    Method.POST, url,
+                    Response.Listener<String> { response ->
+                        Toast.makeText(this, "Correcto", Toast.LENGTH_SHORT).show()
+                        txtnombre.text.clear()
+                        txtcoreo.text.clear()
+                        txtpass.text.clear()
+                    },
+                    Response.ErrorListener { error ->
+                        println("nooo")
+                    }
+                ) {
+                    @Throws(AuthFailureError::class)
+                    override fun getParams(): Map<String, String> {
+                        val params = HashMap<String, String>()
+                        params["nombre"] = name
+                        params["email"] = correo
+                        params["password"] = pass
                 return params
             }
         }
